eb199c839cac49787c4af47f10ba5d7bb07d3181
egor-udalov <zoraudalov@gmail.com>
2024-08-10 09:39:42 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	variable: '--font-nunito',
+	weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+	title: 'Next Pizza | Главная',
+	description: 'Разработка FullStack интернет-магазина (клон ДодоПиццы)',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={nunito.className}>
+				<main className='nin-h-screen'>{children}</main>
+			</body>
+		</html>
+	);
+}
